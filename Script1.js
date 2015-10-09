e62103c98bd28d75886b07c8ef1ea8a8f52e0647
Lynx Webbyrå <jesper.sundstrom@hotmail.com>
2015-10-09 09:04:43 +0200

    Projekt läggs in

---
+++ Script1.js
@@ -0,0 +1,34 @@
+window.onresize = footerToBottom;
+window.onload = footerToBottom;
+function footerToBottom() {
+    windosSize = (window.innerHeight);
+    navheight = (document.getElementById("8hd1-2sd1").clientHeight);
+    footersie = (document.getElementById("j890-c19f").clientHeight);
+    var height = (windosSize - navheight - footersie);
+    document.getElementById("af20-v431").style.minHeight = height;
+
+}
+function showNav() {
+    var c = document.getElementById('burgerMenu');
+
+    if (c.checked) {
+        var elements = document.querySelectorAll('.navContent');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.display = "none";
+        }
+        return true;
+    } else {
+
+        var elements = document.querySelectorAll('.navContent');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.display = "block";
+        }
+
+        return false;
+    }
+
+
+
+
+
+}
